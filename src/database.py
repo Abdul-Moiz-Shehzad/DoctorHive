e55e72052c6d1ad893b1570e431d613d096a839c
Abdul Moiz <abdulmoizfort@gmail.com>
2025-09-08 23:54:25 +0500

    Update Database config

--- src/database.py
+++ src/database.py
@@ -3,16 +3,17 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import os
+from dotenv import load_dotenv
 # Database connection parameters
+load_dotenv()
 DB_PARAMS = {
-    "dbname": "postgres",
-    "user": "postgres",
-    "password": "6025",
-    "host": "localhost",
-    "port": "5432"
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "postgres"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": os.getenv("DB_PORT", "5432"),
 }
-
 # Create SQLAlchemy engine
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_PARAMS['user']}:{DB_PARAMS['password']}@{DB_PARAMS['host']}:{DB_PARAMS['port']}/{DB_PARAMS['dbname']}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
